c830d11aeba73db0b5fca44743702180dd7abd77
Sandeep Kumar Roy <sandeep.myuniverse@gmail.com>
2020-12-02 11:12:44 +0530

    Mondo url changes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set("debug",true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb+srv://sandeep9905:sandeep123@cluster0.6kdqn.mongodb.net/warbler?retryWrites=true&w=majority",{keepAlive:true, useNewUrlParser: true,useCreateIndex:true, useUnifiedTopology: true});
+mongoose.connect("mongodb connect url",{keepAlive:true, useNewUrlParser: true,useCreateIndex:true, useUnifiedTopology: true});
 module.exports.User = require("./user");
 module.exports.Message = require("./message");
